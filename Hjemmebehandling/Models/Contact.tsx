12b4abca289728618546014b2ef38bf0821a8669
Mikkel Birch <mib@kvalitetsit.dk>
2022-01-06 08:58:50 +0100

    1.1.19) No primary-contact-boolean on contact

--- Hjemmebehandling/Models/Contact.tsx
+++ Hjemmebehandling/Models/Contact.tsx
@@ -2,6 +2,5 @@ export class Contact {
     fullname! : string;
     primaryPhone! : string
     secondaryPhone! : string
-    primaryContact! : boolean;
     affiliation! : string; // f.eks. "kone", "ven", "far" etc
 }
