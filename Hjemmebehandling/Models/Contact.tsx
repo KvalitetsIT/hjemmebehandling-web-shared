3395b1a71213eaf2e2f0090a23683f8fd8678b7b
Mikkel Birch <mib@kvalitetsit.dk>
2022-01-06 10:08:59 +0100

    1.1.20) Make contact all optional

--- Hjemmebehandling/Models/Contact.tsx
+++ Hjemmebehandling/Models/Contact.tsx
@@ -1,6 +1,6 @@
 export class Contact {
-    fullname! : string;
-    primaryPhone! : string
-    secondaryPhone! : string
-    affiliation! : string; // f.eks. "kone", "ven", "far" etc
+    fullname? : string;
+    primaryPhone? : string
+    secondaryPhone? : string
+    affiliation? : string; // f.eks. "kone", "ven", "far" etc
 }
