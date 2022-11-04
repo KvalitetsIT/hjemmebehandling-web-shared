2b9eac2f466abb90170a3e3705db75f7d81738af
sunekochhansen <34162776+sunekochhansen@users.noreply.github.com>
2022-11-04 10:37:09 +0100

    Update BaseModelStatus.tsx

--- Hjemmebehandling/Models/BaseModelStatus.tsx
+++ Hjemmebehandling/Models/BaseModelStatus.tsx
@@ -1,5 +1,7 @@
 export enum BaseModelStatus {
     DRAFT = "DRAFT", //The string value is the bff-name of the status
     ACTIVE = "ACTIVE",
+    RETIRED = "RETIRED",
+    COMPLETED = "COMPLETED",
     UKENDT = "UNKNOWN"
 }
